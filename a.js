f294ddf3f06956476ea2e239895222828200a763
Sebastian Larsson <sebberlarsson@hotmail.com>
2019-06-01 21:16:59 +0200

    Testing

--- a.js
+++ a.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App {
   render() {
     return (
-      <div className="foo bar baz">
+      <div className="foo bar baz bongo">
         <button onClick={() => null}\>
       <\div>
     )
